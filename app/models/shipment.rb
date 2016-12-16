d11b85fbb13639b58bafdc1f79ae3ec0d3c86acc
Kumar Consultants <kumarmconsultants@gmail.com>
2016-12-16 22:30:44 +0800

    Heroku oooooo

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,7 +1,7 @@
 class Shipment < ApplicationRecord
   def self.search(search)
     if search.present?
-      where("trackingNumber @@ :q or destination @@ :q or status @@ :q or lastScan @@ :q", q: search)
+      where("trackingNumber @@ :q", q: search)
     else
       scoped
     end
