e4ba37a72325fc9a33221e784d980b741cf1bcfd
Kumar Consultants <kumarmconsultants@gmail.com>
2016-12-16 19:49:31 +0800

    Fix sQlite LIKE syntax to PostgreSQL ILIKE on prod

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,7 +1,7 @@
 class Shipment < ApplicationRecord
   def self.search(search)
     if search.present?
-      where("lower(trackingNumber) like :q", q: "%#{search.downcase}%")
+      where("lower(trackingNumber) ilike :q", q: "%#{search.downcase}%")
     else
       scoped
     end
