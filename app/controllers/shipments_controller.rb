43a5c579da844d8f7fe4bff32ec57129c3304d66
Kumar Consultants <kumarmconsultants@gmail.com>
2016-12-17 13:18:25 +0800

    Add New column to table in Database

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -77,6 +77,6 @@ class ShipmentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def shipment_params
-      params.require(:shipment).permit(:trackingnumber, :destination, :status, :lastscan)
+      params.require(:shipment).permit(:trackingnumber, :destination, :status, :lastscan, :originlocation)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161216145701) do
+ActiveRecord::Schema.define(version: 20161217051029) do
 
   create_table "shipments", force: :cascade do |t|
     t.string   "trackingnumber"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20161216145701) do
     t.string   "lastscan"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "originlocation"
   end
 
 end
