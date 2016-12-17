a2cff547ddfcd4caebd867619078a368f0abf22d
Kumar Consultants <kumarmconsultants@gmail.com>
2016-12-17 15:42:56 +0800

    Sender and Recipient added to table

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -77,6 +77,6 @@ class ShipmentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def shipment_params
-      params.require(:shipment).permit(:trackingnumber, :destination, :status, :lastscan, :originlocation)
+      params.require(:shipment).permit(:trackingnumber, :destination, :status, :lastscan, :originlocation, :sendername, :receivername)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161217051029) do
+ActiveRecord::Schema.define(version: 20161217073523) do
 
   create_table "shipments", force: :cascade do |t|
     t.string   "trackingnumber"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20161217051029) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.string   "originlocation"
+    t.string   "sendername"
+    t.string   "receivername"
   end
 
 end
